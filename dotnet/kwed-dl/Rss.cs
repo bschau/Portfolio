c61319d25fa71e0b355ee00464c3bdfe32048a7f
Brian Schau <brian@schau.dk>
2020-08-05 21:58:26 +0200

    Make a bit nicer

--- dotnet/kwed-dl/Rss.cs
+++ dotnet/kwed-dl/Rss.cs
@@ -14,11 +14,15 @@ namespace kweddl
 			{
 				var xDocument = XDocument.Load(ms);
 				return (from item
-						in xDocument.Root.Descendants().First(x => "channel".Equals(x.Name.LocalName, StringComparison.CurrentCultureIgnoreCase))
-							.Elements().Where(x => "item".Equals(x.Name.LocalName, StringComparison.CurrentCultureIgnoreCase))
+						in xDocument.Root.Descendants()
+							.First(x => "channel".Equals(x.Name.LocalName, StringComparison.CurrentCultureIgnoreCase))
+							.Elements()
+								.Where(x => "item".Equals(x.Name.LocalName, StringComparison.CurrentCultureIgnoreCase))
 						select new KwedItem(
-							item.Elements().First(x => "link".Equals(x.Name.LocalName, StringComparison.CurrentCultureIgnoreCase)).Value,
-							item.Elements().First(x => "title".Equals(x.Name.LocalName, StringComparison.CurrentCultureIgnoreCase)).Value
+							item.Elements()
+								.First(x => "link".Equals(x.Name.LocalName, StringComparison.CurrentCultureIgnoreCase)).Value,
+							item.Elements()
+								.First(x => "title".Equals(x.Name.LocalName, StringComparison.CurrentCultureIgnoreCase)).Value
 						)
 				).ToList();
 			}
