d336a55844806a78529a6382c6f669b119a98335
Brian Schau <brian@schau.dk>
2020-09-20 07:38:52 +0200

    Refreshed

--- python/Newsfeed/src/sendmail.py
+++ python/Newsfeed/src/sendmail.py
@@ -1,18 +1,15 @@
-""" vpsutils """
+""" sendmail """
 import configparser
 import os
 import platform
 import smtplib
-from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import time
 import requests
 
 
-class VpsUtils():
-    """ VPS Utilities module """
-
+class SendMail():
+    """ SendMail module """
 
     def __init__(self, module):
         """ Constructor.
@@ -28,26 +25,6 @@ class VpsUtils():
         else:
             self.prefix = "."
             self.tmp = "/tmp"
-
-        self.reset_html()
-
-
-    def reset_html(self):
-        """ Reset HTML. """
-        self.html = []
-        self.now = time.localtime()
-        self.title = "{0} {1}".format(self.module,
-                                      time.strftime('%Y-%m-%d %H:%M:%S',
-                                                    self.now))
-        self.html.append("""<!DOCTYPE html>
-<html dir="ltr" lang="en">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta name="viewport" content="width=device-width" />
-    <title>""")
-        self.html.append(self.title + """</title>
-</head>
-<body>""")
 
 
     @staticmethod
@@ -69,40 +46,33 @@ class VpsUtils():
         return self.config[section][key]
 
 
-    def deliver(self, att_data=False, att_filename=""):
+    def deliver(self, title, body):
         """ Delivery email.
-        	Arguments:
-            	att_data: optional attachment data
-                att_filename: optional attachment filename
+            Arguments:
+                title: mail title
+                body: body text
         """
-        self.html.append("</body></html>")
-        html = self.html_to_string()
-
         method = self.config['GENERAL']['method']
         if method == "file":
             path = self.config['FILE']['path']
             with open(path, "w") as html_file:
-                html_file.write(html)
+                html_file.write(body)
             return
 
         text = "Please read this in a HTML mail user agent."
         if method == "smtp":
+            recipients = self.get_recipients('SMTP')
             message = MIMEMultipart('alternative')
-            message['Subject'] = self.title
+            message['Subject'] = title
             message['From'] = self.config['SMTP']['from']
-            message['To'] = self.config['SMTP']['to']
+            message['To'] = ", ".join(recipients)
             plain_text = MIMEText(text, 'plain')
-            html_text = MIMEText(html, 'html')
+            html_text = MIMEText(body, 'html')
             message.attach(plain_text)
             message.attach(html_text)
 
-            if att_data:
-                part = MIMEApplication(att_data, att_filename)
-                part['Content-Disposition'] = 'attachment; filename="%s"' % att_filename
-                message.attach(part)
-
             mail = smtplib.SMTP('localhost')
-            mail.send_message(message)
+            mail.sendmail(message['From'], recipients, message.as_string())
             mail.quit()
             return
 
@@ -110,22 +80,27 @@ class VpsUtils():
             self.config['MAILGUN']['url'],
             auth=("api", self.config['MAILGUN']['auth']),
             data={"from": self.config['MAILGUN']['from'],
-                  "to": self.config['MAILGUN']['to'],
-                  "subject": self.title,
+                  "to": self.get_recipients('MAILGUN'),
+                  "subject": title,
                   "text": text,
-                  "html": html
+                  "html": body
                  },
         )
 
 
-    def html_to_string(self):
-        """ Return HTML buffer as string. """
-        return ''.join(self.html)
-
-
-    def html_add(self, string):
-        """ Add string to HTML buffer.
+    def get_recipients(self, section):
+        """ Get the list of recipients.
             Arguments:
-                string: string with formatted HTML to add
+                section: major section for initial recipient.
         """
-        self.html.append(string)
+        recipients = []
+        for recipient in self.config[section]['to'].split(','):
+            recipients.append(recipient.strip())
+
+        if self.module not in self.config.sections():
+            return recipients
+
+        for recipient in self.config[self.module]['additional_to'].split(','):
+            recipients.append(recipient.strip())
+
+        return recipients
