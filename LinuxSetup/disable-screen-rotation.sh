ab5bcf69cb7937caa744c9a60e67a62dbdb7e137
Brian Schau <brian.schau@visma.com>
2020-11-19 10:38:03 +0100

    Added Linux setup files

---
+++ LinuxSetup/disable-screen-rotation.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gsettings set org.gnome.settings-daemon.peripherals.touchscreen orientation-lock true
+gsettings set org.gnome.settings-daemon.plugins.orientation active false
---
+++ LinuxSetup/restore.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+sources="00-OurLife 00-Depot"
+dryrun="--dry-run"
+slow=""
+
+if test -s "/home/bs/.rsyncrc"; then
+	. "/home/bs/.rsyncrc"
+else
+	echo "/Home/bs/.rsyncrc not found" >&2
+	exit 1
+fi
+
+if test -z "$RSYNCACCOUNT"; then
+	echo "RSYNCACCOUNT not set" >&2
+	exit 1
+fi
+
+for arg in "$@"; do
+	if test "$arg" = "--for-real"; then
+		dryrun=""
+	elif test "$arg" = "--slow"; then
+		slow="yes"
+	fi
+done
+
+if test -n "$slow"; then
+	echo "Running in slow mode."
+	mode="--ignore-times"
+else
+	mode="--omit-dir-times --size-only"
+fi
+
+if test -n "$dryrun"; then
+	echo "Running in dry-run mode. Invoke this script with the --for-real argument"
+	echo "to actually perform the transfers:"
+	echo ""
+	echo "        $0 --for-real"
+	echo ""
+	echo ""
+	echo "Press enter to continue ..."
+	read line
+fi
+
+rsync_flags="$dryrun -rvz -zz --progress $mode --delete --exclude=.Spotlight-V100 --exclude=.DocumentRevisions-V100 --exclude=.DS_Store --exclude=.fseventsd --exclude=.Trashes --exclude=.TemporaryItems --exclude=.git --exclude=.gitignore --exclude=.gd --exclude=*~"
+
+#
+# Restore from remote
+#
+for src in $sources; do
+	dir="${HOME}/${src}"
+	remote="${RSYNCACCOUNT}:${src}"
+	echo "Syncing from $remote to $dir..."
+	rsync $rsync_flags --rsh=ssh "${remote}/" "${dir}/"
+done
+exit 0
