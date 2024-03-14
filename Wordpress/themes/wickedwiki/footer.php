f35d2beaccf32af64ee95f1cb42f22354cbf9f28
Brian Schau <brian@schau.dk>
2024-03-14 19:17:06 +0100

    Updated with prod

--- Wordpress/themes/wickedwiki/footer.php
+++ Wordpress/themes/wickedwiki/footer.php
@@ -1,7 +1,7 @@
 <p><br /></p>
 <p><br /></p>
 <p><br /></p>
-<p style="text-align: center">Proudly powered by Wordpress and Wicked Wiki! (<a href="/wp-admin/">Login</a>)</p>
+<p id="footer">Proudly powered by Wordpress and Wicked Wiki! (<a href="/wp-admin/">Login</a>)</p>
 <p><br /></p>
 </body>
 </html>
